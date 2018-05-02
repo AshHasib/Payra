2b8835e07cb62d9ed886ec16469878a5fcdafcbd
AshHasib <30811301+AshHasib@users.noreply.github.com>
2018-05-02 13:02:43 +0600

    Add files via upload

---
+++ MyChattingApp/src/mychattingapp/MyChattingApp.java
@@ -0,0 +1,15 @@
+
+package mychattingapp;
+
+import mychattingapp.gui.MainScreen;
+
+
+public class MyChattingApp {
+
+    
+    public static void main(String[] args) {
+        MainScreen screen=new MainScreen();
+        screen.show();
+    }
+    
+}
---
+++ MyChattingApp/src/mychattingapp/networking/MessageTransmitter.java
@@ -0,0 +1,38 @@
+
+package mychattingapp.networking;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Hasib Ashaduzzaman
+ */
+public class MessageTransmitter extends Thread{
+    
+    String message,hostname;
+    
+    int port;
+    
+    public MessageTransmitter(String message,String hostname,int port)
+    {
+        this.message=message;
+        this.hostname=hostname;
+        this.port=port;
+    }
+    
+    @Override
+    public void run()
+    {
+        try {
+            Socket s=new Socket(hostname,port);
+            s.getOutputStream().write(message.getBytes());
+            s.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(MessageTransmitter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
